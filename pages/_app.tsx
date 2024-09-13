c7b4488086ccd19210eafbec4cf213be8cb72ba1
Adams Kingsley <Yelsgink@gmail.com>
2024-09-13 09:31:44 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,25 @@
 import "@/styles/globals.css";
+import Header from '../components/Header'; 
+import '../components/ShimmerWidget.css';
 import type { AppProps } from "next/app";
+import { ItemProvider } from '@/context/ItemProvider';
+import { FarmProvider } from '@/context/FarmProvider';
+import { AuthProvider } from "@/context/AuthProvider";
+import Footer from "@/components/Footer";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <AuthProvider>
+      <FarmProvider>
+        <ItemProvider>
+          <Header />
+          <main className="bg-white ">
+            <Component {...pageProps} />
+          </main>
+          <Footer/>
+          {/* Optionally, you can add a Footer component here */}
+        </ItemProvider>
+      </FarmProvider>
+    </AuthProvider>
+  );
 }

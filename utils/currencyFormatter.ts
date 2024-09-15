ccb97f327289b69b56faf01343b9aefe448f92e4
Adams Kingsley <Yelsgink@gmail.com>
2024-09-15 13:12:15 +0100

    Create currencyFormatter.ts

---
+++ utils/currencyFormatter.ts
@@ -0,0 +1,8 @@
+export const formatCurrency = (amount: number, currency: string = 'NGN'): string => {
+  return new Intl.NumberFormat('en-NG', {
+    style: 'currency',
+    currency: currency,
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(amount);
+};
